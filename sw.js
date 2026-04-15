f9e251b2d2fdb9fb5a570c1fe4d94fea387cf40e
Anasalsheikhpp <anasalsheikhpp@gmail.com>
2026-04-15 23:04:06 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,24 @@
 const CACHE_NAME = 'anas-store-v1';
 const ASSETS = [
-  '/anas-store-app/',
-  '/anas-store-app/index.html',
-  '/anas-store-app/manifest.json'
-];
+    './',
+    'index.html',
+    'manifest.json',
+    'app_icon_192x192.png',
+    'app_icon_512x512.png'
+  ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+    event.waitUntil(
+          caches.open(CACHE_NAME).then((cache) => {
+                  return cache.addAll(ASSETS);
+          })
+        );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+          caches.match(event.request).then((response) => {
+                  return response || fetch(event.request);
+          })
+        );
 });
